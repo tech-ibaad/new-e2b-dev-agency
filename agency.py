543305bbf0889f47ebc4e4b63571f304bbbc8ba5
Mike Dev <mykhailo.shchuka.business@gmail.com>
2025-10-11 03:54:20 +0300

    Add new agent

--- agency.py
+++ agency.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from agency_swarm import Agency
 
 from example_agent import example_agent
-from example_agent2 import example_agent2
 
 import asyncio
 
@@ -11,8 +10,7 @@ load_dotenv()
 # do not remove this method, it is used in the main.py file to deploy the agency (it has to be a method)
 def create_agency(load_threads_callback=None):
     agency = Agency(
-        example_agent, example_agent2,
-        communication_flows=[(example_agent, example_agent2)],
+        example_agent,
         name="ExampleAgency", # don't forget to rename your agency!
         shared_instructions="shared_instructions.md",
         load_threads_callback=load_threads_callback,
